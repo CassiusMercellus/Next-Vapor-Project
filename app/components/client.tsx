6847688571c078228860bc2d5e3f9aac10e4834b
Cassius Hill <MarcelusWork@gmail.com>
2024-07-04 23:08:35 -0600

    finished filter

--- app/components/client.tsx
+++ app/components/client.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import { IoGameController  } from "react-icons/io5";
 import { IoIosNotifications  } from "react-icons/io";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-import { FaUserFriends } from "react-icons/fa";
+import { FaUserFriends, FaAngleDown } from "react-icons/fa";
 import PFP from "../pfp.png";
 
 
@@ -23,17 +23,19 @@ export default function Client() {
                 <Link href="/" className="px-2 py-4 font-bold rounded-lg hover:text-blue-400">Downloads</Link>
                 
                 <div className="flex flex-row">
-                    <Link href="/" className="flex px-4 py-1 my-2 mx-1 items-center rounded-lg bg-slate-800 hover:bg-slate-700"><FaUserFriends size={20}/></Link>
-                    <Link href="/" className="flex px-4 py-1 my-2 mx-1  items-center rounded-lg bg-slate-800 hover:bg-slate-700"><IoIosNotifications size={22}/></Link>
-                    <div className="flex flex-row px-1 py-1 my-2 mx-1 rounded-lg bg-slate-800 hover:bg-slate-700">
+                    <Link href="/" className="flex px-4 py-1 my-2 mx-1 items-center rounded-md bg-slate-800 hover:bg-slate-700"><FaUserFriends size={20}/></Link>
+                    <Link href="/" className="flex px-4 py-1 my-2 mx-1  items-center rounded-md bg-slate-800 hover:bg-slate-700"><IoIosNotifications size={22}/></Link>
+                    <div className="flex flex-row px-1 py-1 my-2 mx-1 rounded-md bg-slate-800 hover:bg-slate-700 items-center h-10">
                         <Image
-                            className="rounded-full w-full mx-2" 
+                            className="rounded-lg m-1" 
                             src={PFP} 
                             alt="Picture of the user"
-                            width={25} 
-                            height={25}  
+                            width={35} 
+                            height={35}  
                         />
                         <p className="flex items-center mx-2">LoginName</p>
+                        <p className="flex items-center mx-2 text-gray-500">$0.00</p>
+                        <FaAngleDown size={40}  className="h-5"/>
                     </div>
                 </div>
                 <div className="flex flex-row">
--- app/components/filters.tsx
+++ app/components/filters.tsx
@@ -1,11 +1,12 @@
 import { FaSearch } from "react-icons/fa";
+import { MdOutlineFilterAltOff } from "react-icons/md";
 
 export default function Filters() {
     return(
         <div className="flex flex-row bg-slate-900 justify-between p-5 gap-3.5">
             <div className="flex flex-col w-1/3 gap-2.5">
                 <div className="flex flex-col">
-                    <h2 className="my-2.5">Type</h2>
+                    <h2 className="my-2.5 text-lg">Type</h2>
                     <div className="pl-3">
                         <p className="h-8">Games</p>
                         <p className="h-8">Software</p>
@@ -16,7 +17,7 @@ export default function Filters() {
                 </div>
 
                 <div className="flex flex-col border-t-2 border-slate-700">
-                    <h2 className="my-2.5">Players</h2>
+                    <h2 className="my-2.5 text-lg">Players</h2>
                     <div className="pl-3">
                         <p className="h-8">Single Player</p>
                         <p className="h-8">Multiplayer</p>
@@ -25,7 +26,7 @@ export default function Filters() {
                 </div>
 
                 <div className="flex flex-col border-t-2 border-slate-700">
-                    <h2 className="my-2.5">Play State</h2>
+                    <h2 className="my-2.5 text-lg">Play State</h2>
                     <div className="pl-3">
                         <p className="h-8">Ready to Play</p>
                         <p className="h-8">Installed Locally</p>
@@ -38,7 +39,7 @@ export default function Filters() {
             
             <div className="flex flex-col w-1/3 gap-2.5">
                 <div className="flex flex-col">
-                    <h2 className="my-2.5">Hardware Support</h2>
+                    <h2 className="my-2.5 text-lg">Hardware Support</h2>
                     <div className="pl-3">
                         <p className="h-8">Controllers (full)</p>
                         <p className="h-8">Controllers (partial)</p>
@@ -47,7 +48,7 @@ export default function Filters() {
                 </div>
 
                 <div className="flex flex-col border-t-2 border-slate-700">
-                    <h2 className="my-2.5">Features</h2>
+                    <h2 className="my-2.5 text-lg">Features</h2>
                     <div className="pl-3">
                         <p className="h-8">Trading Cards</p>
                         <p className="h-8">Workshop</p>
@@ -57,38 +58,41 @@ export default function Filters() {
                 </div>
 
                 <div className="flex flex-col border-t-2 border-slate-700">
-                    <h2 className="my-2.5">Friends</h2>
+                    <h2 className="my-2.5 text-lg">Friends</h2>
                     <div className="flex flex-row justify-between items-center rounded-sm p-2 bg-blue-600 opacity-25">
-                        <p className="text-blue-900 opacity-100">Enter Friend's Name</p>
+                        <input type="text" className="text-white opacity-100 bg-blue-600 outline-none w-full" placeholder="Search for a tag..." />
                         <FaSearch />
                     </div>
                 </div>
                 <div className="flex flex-col border-t-2 border-slate-700">
-                    <h2 className="my-2.5">Store Tags</h2>
+                    <h2 className="my-2.5 text-lg">Store Tags</h2>
                     <div className="flex flex-row justify-between items-center rounded-sm p-2 bg-blue-600 opacity-25">
-                        <p className="text-blue-900 opacity-100">Search for a tag...</p>
+                        <input type="text" className="text-white opacity-100 bg-blue-600 outline-none w-full" placeholder="Search for a tag..." />
                         <FaSearch />
                     </div>
                 </div>
             </div>
 
-            <div className="flex flex-col w-1/3">
-                <h2 className="mb-2">Genre</h2>
-                <div className="flex flex-col pl-3">
-                    <p className="h-8">Action</p>
-                    <p className="h-8">Adventure</p>
-                    <p className="h-8">Casual</p>
-                    <p className="h-8">Massively Multiplayer</p>
-                    <p className="h-8">Racing</p>
-                    <p className="h-8">RPG</p>
-                    <p className="h-8">Simulation</p>
-                    <p className="h-8">Sports</p>
-                    <p className="h-8">Strategy</p>
+            <div className="flex flex-col w-1/3 justify-between">
+                <div>
+                    <h2 className="mb-2 text-lg">Genre</h2>
+                    <div className="flex flex-col pl-3">
+                        <p className="h-8">Action</p>
+                        <p className="h-8">Adventure</p>
+                        <p className="h-8">Casual</p>
+                        <p className="h-8">Massively Multiplayer</p>
+                        <p className="h-8">Racing</p>
+                        <p className="h-8">RPG</p>
+                        <p className="h-8">Simulation</p>
+                        <p className="h-8">Sports</p>
+                        <p className="h-8">Strategy</p>
+                    </div>
                 </div>
-                <div className="">
-                <h2>Sort/filter Icon</h2>
-                <button>Create Collection</button>
-            </div>
+                
+                <div className="flex flex-row justify-center">
+                    <MdOutlineFilterAltOff size={50} className="rounded-md p-2 bg-gray-800 text-white hover:bg-gray-700" />
+                    <button className="rounded-md p-2 px-6 bg-blue-500 text-white ml-6 hover:bg-blue-600">Create Collection</button>
+                </div>
             </div>
             
         </div>
